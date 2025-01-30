21b87af2049e9b51afbc959d1b11cf5d8d33834e
KutilT23 <kutil@spsejecna.cz>
2025-01-30 10:12:34 +0100

    Metody na zasazení a sklizení kytek

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
+
 public class Farm {
 
+
+    private double farmSize = 100;
+    private ArrayList<Flower>farm = new ArrayList<>();
+    private ArrayList<Animal>barn = new ArrayList<>();
+    private double usedSpaceFarm;
+
+    public void harvestFlower(String name){
+        for (int i = 0; i < farm.size(); i++) {
+        if(farm.get(i).name.equals(name)){
+            farm.remove(i);
+            System.out.println("Successfully harvested flower");
+         }else{
+            System.out.println("Failed to harvest flower");
+        }
+        }
+
+    }
+    public void plantFlower(Flower flower){
+            if(flower.neededArea+usedSpaceFarm <= farmSize&&farm.size()<5){
+                farm.add(flower);
+                usedSpaceFarm = usedSpaceFarm + flower.neededArea;
+                System.out.println("Successfully added flower");
+            }else{
+                System.out.println("Failed to add flower");
+            }
+
+
+    }
+    public void waterFlower(){
+
+    }
+    public void removeFlower(){
+
+    }
+    public void buyAnimal(){
+
+    }
+    public void petAnimal(){
+
+    }
+    public void sellAnimal(){
+
+    }
 
 
 }
