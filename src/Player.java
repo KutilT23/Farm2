7caf2410fa3a2e87aa8aa54255f257de37c45120
KutilT23 <kutil@spsejecna.cz>
2025-01-30 10:26:55 +0100

    Nova class na penize a jmeno hrace

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+    private String name = "";
+    private double currency = 1000;
+
+
+    public double getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(double currency) {
+        this.currency = currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
