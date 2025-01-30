9cc86d9e6030ee35f3b321a4f66dd9334b3c5c1f
KutilT23 <kutil@spsejecna.cz>
2025-01-30 10:33:47 +0100

    Upravení metod harvestFlower, waterFlower,removeFlower a vytvoreni nove metody addAnimalsToBarn

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Farm {
 
@@ -7,12 +8,22 @@ public class Farm {
     private ArrayList<Flower>farm = new ArrayList<>();
     private ArrayList<Animal>barn = new ArrayList<>();
     private double usedSpaceFarm;
+    Player player = new Player();
+    private int chance = 0;
 
     public void harvestFlower(String name){
         for (int i = 0; i < farm.size(); i++) {
         if(farm.get(i).name.equals(name)){
             farm.remove(i);
-            System.out.println("Successfully harvested flower");
+            Random rd = new Random();
+            chance = rd.nextInt(1,101);
+            if(chance >=50){
+                System.out.println("Successfully harvested flower");
+                player.setCurrency(player.getCurrency()+75);
+            }else{
+                System.out.println("Plant did not grow up");
+            }
+
          }else{
             System.out.println("Failed to harvest flower");
         }
@@ -24,6 +35,8 @@ public class Farm {
                 farm.add(flower);
                 usedSpaceFarm = usedSpaceFarm + flower.neededArea;
                 System.out.println("Successfully added flower");
+                player.setCurrency(player.getCurrency()-50);
+
             }else{
                 System.out.println("Failed to add flower");
             }
@@ -31,9 +44,20 @@ public class Farm {
 
     }
     public void waterFlower(){
-
+    chance = chance+20;
     }
-    public void removeFlower(){
+    public void removeFlower(String name){
+        for (int i = 0; i <farm.size() ; i++) {
+            if(farm.get(i).name.equals(name)){
+                farm.remove(i);
+                System.out.println("Successfully removed flower, but no money :(");
+            }else{
+                System.out.println("Failed to destroy your flower");
+            }
+
+        }
+    }
+    public void addAnimalsToBarn(){
 
     }
     public void buyAnimal(){
